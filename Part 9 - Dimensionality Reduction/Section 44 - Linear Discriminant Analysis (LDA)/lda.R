e787235981cbc69f95cb4719311da7e8fdc45ea5
Vaibhav Kulshrestha <vaibhav1kulshrestha@gmail.com>
2019-06-27 20:26:32 +0530

    Removed extra line breaks from some files

--- Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda.R
+++ Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/lda.R
@@ -4,7 +4,7 @@
 dataset = read.csv('Wine.csv')
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+install.packages('caTools')
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Customer_Segment, SplitRatio = 0.8)
@@ -24,12 +24,9 @@ test_set = as.data.frame(predict(lda, test_set))
 test_set = test_set[c(5, 6, 1)]
 
 # Fitting SVM to the Training set
-# install.packages('e1071')
+install.packages('e1071')
 library(e1071)
-classifier = svm(formula = class ~ .,
-                 data = training_set,
-                 type = 'C-classification',
-                 kernel = 'linear')
+classifier = svm(formula = class ~ ., data = training_set, type = 'C-classification', kernel = 'linear')
 
 # Predicting the Test set results
 y_pred = predict(classifier, newdata = test_set[-3])
@@ -45,10 +42,7 @@ X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3],
-     main = 'SVM (Training set)',
-     xlab = 'LD1', ylab = 'LD2',
-     xlim = range(X1), ylim = range(X2))
+plot(set[, -3], main = 'SVM (Training set)', xlab = 'LD1', ylab = 'LD2', xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
 points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
@@ -61,9 +55,7 @@ X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
-plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'LD1', ylab = 'LD2',
-     xlim = range(X1), ylim = range(X2))
+plot(set[, -3], main = 'SVM (Test set)', xlab = 'LD1', ylab = 'LD2', xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
 points(set, pch = 21, bg = ifelse(set[, 3] == 2, 'blue3', ifelse(set[, 3] == 1, 'green4', 'red3')))
